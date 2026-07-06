40b34cc8c8ae2ae7e390c4f978032c3b601b3750
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-07-06 14:04:58 +0100

    Update PopupHistoriqueDistribution.jsx

--- src/components/PopupHistoriqueDistribution.jsx
+++ src/components/PopupHistoriqueDistribution.jsx
@@ -64,6 +64,7 @@ const PopuphistoriqueDistribution = ({
                 <SearchBar
                   placeholder="Cherchez ici"
                   width="w-full"
+                  maxWidth="max-w-none"
                   showFilter={false}
                   value={search}
                   onChange={(e) => setSearch(e.target.value)}
