25e1666388ff2eb8cb19cc81136915bec661253d
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-07-06 22:42:14 +0100

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,63 +6,66 @@ const Navigation = ({
   onNext,
   backText = "Back",
   nextText = "Suivant",
+  showBack = true,
 }) => {
   return (
     <div
       className="
         w-full
-
         px-4
         lg:pl-50
         lg:pr-5
-
         py-2
       "
     >
       <div
         className="
           h-[45px]
-
           flex
           items-center
           justify-between
         "
       >
         {/* Back */}
-        <button
-          type="button"
-          onClick={onBack}
-          className="
-            flex
-            items-center
-            gap-1.5
+        {showBack ? (
+          <button
+            type="button"
+            onClick={onBack}
+            className="
+              flex
+              items-center
+              gap-1.5
 
-            text-black
-            font-medium
+              text-black
+              font-medium
 
-            text-[16px]
-            sm:text-[17px]
-            lg:text-[18px]
+              text-[16px]
+              sm:text-[17px]
+              lg:text-[18px]
 
-            cursor-pointer
+              cursor-pointer
 
-            transition-all
-            duration-150
-            ease-in-out
+              transition-all
+              duration-150
+              ease-in-out
 
-            hover:opacity-80
+              hover:opacity-80
 
-            active:scale-[0.98]
-            active:opacity-60
-          "
-        >
-          <img
-            src={ArrowLeft}
-            alt="Back"
-            className="w-[10px] h-[10px]"
-          />
-          <span>{backText}</span>
-        </button>
+              active:scale-[0.98]
+              active:opacity-60
+            "
+          >
+            <img
+              src={ArrowLeft}
+              alt="Back"
+              className="w-[10px] h-[10px]"
+            />
+
+            <span>{backText}</span>
+          </button>
+        ) : (
+          <div />
+        )}
 
         {/* Suivant */}
         <button
@@ -93,6 +96,7 @@ const Navigation = ({
           "
         >
           <span>{nextText}</span>
+
           <img
             src={ArrowRight}
             alt="Suivant"
