cf8289603e746c93adbf9663377abcf6a595bdc0
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-07-06 14:15:35 +0100

    Update cardDistribution.jsx

--- src/components/cardDistribution.jsx
+++ src/components/cardDistribution.jsx
@@ -16,12 +16,12 @@ const CardPopupDistribution = ({
     >
       {/* Ligne 1 */}
       <div className="flex justify-between items-start">
-        <h2 className="text-[18px] font-bold text-[#1E1E1E]">
+        <h2 className="text-[16px] sm:text-[18px] md:text-[20px] font-bold text-[#1E1E1E]">
           {enfant}
         </h2>
 
         <span
-          className="text-[18px] font-bold"
+          className="text-[15px] sm:text-[16px] md:text-[18px] font-bold"
           style={{ color: "#528583" }}
         >
           {code}
@@ -30,30 +30,30 @@ const CardPopupDistribution = ({
 
       {/* Ligne 2 */}
       <div className="flex justify-between items-center mt-4">
-        <span className="text-[18px] font-semibold text-[#1E1E1E]">
+        <span className="text-[15px] sm:text-[16px] md:text-[18px] font-semibold text-[#1E1E1E]">
           {distribution}
         </span>
 
-        <span className="text-[18px] text-[#1E1E1E]">
+        <span className="text-[15px] sm:text-[16px] md:text-[18px] text-[#1E1E1E]">
           {date}
         </span>
       </div>
 
       {/* Produits dynamiques */}
-      <div className="mt-4 space-y-2">
+      <div className="mt-4 space-y-0.5">
         {produits.map((produit, index) => (
           <div
             key={index}
             className="flex items-center"
           >
             <span
-              className="text-[16px]"
+              className="text-[14px] sm:text-[15px] md:text-[16px]"
               style={{ color: "#4E9F8A" }}
             >
               {produit.nom}
             </span>
 
-            <span className="ml-4 text-[18px] text-[#1E1E1E]">
+            <span className="ml-4 text-[15px] sm:text-[16px] md:text-[18px] text-[#1E1E1E]">
               {produit.quantite}
             </span>
           </div>
