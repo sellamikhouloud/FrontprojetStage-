b2a4358e5e39fd96e08de4e14c9cba459b38c15a
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-07-29 00:05:45 +0100

    Update ListeZakat.jsx

--- src/Zakat/ListeZakat.jsx
+++ src/Zakat/ListeZakat.jsx
@@ -10,6 +10,7 @@ import CardListZakat from "../components/Cards/CarteListeZakat";
 import SelectInput  from "../components/Containers/ChoiceContainer";
 import PopupDetailZakat from "../components/Popups/PopupdetailsZakat";
 import PopupModifierZakat from "../components/Popups/PopupdetailsZakatModifier";
+import  PopupAlimenterSolde from "../components/Popups/PopupAlimenterSolde";
 import NoResultImage from "../assets/no result picture.svg";
 import { useNavigate } from "react-router-dom";
 
@@ -17,7 +18,7 @@ export default function ZakatPage() {
   const [search, setSearch] = useState("");
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [showStockPopup, setShowStockPopup] = useState(false);
-
+ 
 
   const navigate = useNavigate();
 const [filters, setFilters] = useState({
@@ -106,6 +107,7 @@ const [selectedZakat, setSelectedZakat] = useState(null);
 const [showDetailPopup, setShowDetailPopup] = useState(false);
 
 const [openModifier, setOpenModifier] = useState(false);
+ const [openAlimenterSolde, setOpenAlimenterSolde] = useState(false);
 const [isMobile, setIsMobile] = useState(
   window.innerWidth < 768
 );
@@ -199,13 +201,9 @@ if (isFilterOpen && isMobile) {
       <main className="flex-1 overflow-y-auto px-5 pt-18 md:pt-0 pb-8 lg:p-10 bg-white">
        
 <NavigationHeader
- 
-
- 
-
   secondType="add"
   secondActionTitle="Alimenter le solde"
-  onSecondAction={() => navigate("")}
+  onSecondAction={() => setOpenAlimenterSolde(true)}
 />
  
 <NavigationHeader
@@ -297,6 +295,16 @@ if (isFilterOpen && isMobile) {
         setOpenModifier(false);
         setShowDetailPopup(true);
     }}
+/>
+
+<PopupAlimenterSolde
+  open={openAlimenterSolde}
+  onClose={() => setOpenAlimenterSolde(false)}
+  onSave={(data) => {
+    console.log(data);
+
+    setOpenAlimenterSolde(false);
+  }}
 />
     </div>
    
