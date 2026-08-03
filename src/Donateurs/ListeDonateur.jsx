24e981ee4879c393750dca52abd1bf58e24b4266
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-08-03 18:45:18 +0100

    Update ListeDonateur.jsx

--- src/Donateurs/ListeDonateur.jsx
+++ src/Donateurs/ListeDonateur.jsx
@@ -99,7 +99,7 @@ export default function ListeDonateur() {
         <NavigationHeader
   title="Liste des donateurs"
 
-  type="share"
+  type="export"
   actionTitle="Exporter la liste des donateurs"
   onAction={() => console.log("Exporter")}
 
