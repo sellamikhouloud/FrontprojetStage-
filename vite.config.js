d7840eb1175db85555f30325eec4e656619c763d
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-08-13 16:59:03 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  assetsInclude: ['**/*.PNG'],
-  //server: {
-  //  host: '192.168.100.12',
-  //  port: 5173,
-  //}
-})
+
+  assetsInclude: ["**/*.PNG"],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  // server: {
+  //   host: "192.168.100.12",
+  //   port: 5173,
+  // },
+});
