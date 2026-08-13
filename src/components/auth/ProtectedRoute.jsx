1d82ae12ee269d86f56f23fa43dd27b1da3c04d5
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-08-13 17:01:33 +0100

    Update ProtectedRoute.jsx

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 /*
 import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../providers/AuthProvider";
+import { useAuth } from "@/components/providers/AuthProvider";
 import Spinner from "../Spinner";
 
 
