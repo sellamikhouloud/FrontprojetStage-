dd6d925f18df1b22951daf27510db994e697c4d5
GhazliDarine <nm_ghazli@esi.dz>
2026-08-18 12:00:28 +0100

    Add createUser

--- src/lib/api/coordinateurs.jsx
+++ src/lib/api/coordinateurs.jsx
@@ -1,3 +1,5 @@
 import api from "@/lib/axios";
 
 export const listCoordinateurs = (params) => api.get("/api/users/lister_coordinateurs/", { params });
+
+export const createUser = (payload) => api.post("/api/users/", payload);
