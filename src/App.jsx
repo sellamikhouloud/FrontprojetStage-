cd92f10983297c80c7327ed3d6b4b1c4b863a817
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-08-19 16:10:22 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ function App() {
          <Route
   path="/liste-distributions"
   element={
-    <ProtectedRoute allowedRoles={["admin", "chef_coordinator"]}>
+    <ProtectedRoute>
       <DistributionPage />
     </ProtectedRoute>
   }
