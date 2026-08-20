f7de21b70a184d60577ae045e68b5c4d2589404c
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-08-21 00:05:48 +0100

    Update stock.jsx

--- src/lib/api/stock.jsx
+++ src/lib/api/stock.jsx
@@ -10,3 +10,8 @@ export const updateProduit = (id, patch) => api.patch(`/api/produits/${id}/`, pa
 export const ajouterStock = (id, payload) => api.patch(`/api/produits/${id}/ajouter-stock/`, payload);
 
 export const modifierSeuil = (id, payload) => api.patch(`/api/produits/${id}/modifier_seuil/`, payload);
+
+export const listStock = (params) => api.get("/api/produits/", { params });
+
+export const CreateProduit  = (payload) => api.post("/api/produits/", payload);
+
