1c1aa65c3bb6ad74eda4a4cdb4b99ce3b19239d3
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-08-24 19:30:24 +0100

    Update Parametres.jsx

--- src/lib/api/Parametres.jsx
+++ src/lib/api/Parametres.jsx
@@ -3,3 +3,5 @@ import api from "@/lib/axios";
 export const listVillages = () => api.get("/api/villages/");
 
 export const getTauxDeChange = () => api.get("/api/parametres/taux-de-change/");
+
+export const getEmailsRapport = () => api.get("/api/parametres/emails-rapport/");
