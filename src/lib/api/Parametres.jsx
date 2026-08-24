2cf734c8a8b27cba8ccc6cc4308eb04add284616
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-08-24 22:42:33 +0100

    Update Parametres.jsx

--- src/lib/api/Parametres.jsx
+++ src/lib/api/Parametres.jsx
@@ -4,4 +4,4 @@ export const listVillages = () => api.get("/api/villages/");
 
 export const getTauxDeChange = () => api.get("/api/parametres/taux-de-change/");
 
-export const getEmailsRapport = () => api.get("/api/parametres/emails-rapport/");
+export const getEmailsRapport = (params) => api.get("/api/parametres/emails-rapport/", { params });
