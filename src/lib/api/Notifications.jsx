ab701b401f7382e29eccc8dd8d5cc32ad1a3ed79
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-08-26 18:01:56 +0100

    Update Notifications.jsx

--- src/lib/api/Notifications.jsx
+++ src/lib/api/Notifications.jsx
@@ -2,6 +2,6 @@ import api from "@/lib/axios";
 
 export const getNotifications = () => api.get("/api/alertes/notifications/");
 
-export const getHistoriqueAlertes = () => api.get("/api/alertes/historique/");
+export const getHistoriqueAlertes = (params) =>api.get("/api/alertes/historique/", { params });
 
 export const exportHistoriqueAlertes = () => api.get("/api/alertes/historique/export/", {  responseType: "blob", });
