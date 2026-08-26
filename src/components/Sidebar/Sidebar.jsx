01f3217ba0a75f3d9274d2e1b303a2bc22207436
GhazliDarine <nm_ghazli@esi.dz>
2026-08-26 16:47:44 +0100

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -48,15 +48,16 @@ export default function Sidebar({
   const isAdmin = role === "admin";
   const isCoordinator = role === "coordinator";
 
-  const adminData = {
-    nom: user?.nom || "Admin",
-    id: user?.id ? `id – ${user.id}` : "id – admin",
-    role: "Admin",
-    avatarUrl: user?.profilePicture,
-    email: user?.email,
-    telephone: user?.telephone,
-    region: user?.region,
-  };
+
+const adminData = {
+  nom: user?.nom ?? "",
+  prenom: user?.prenom ?? "",
+  username: user?.username,
+  id: user?.id ? `id – ${user.id}` : "id – admin",
+  role: "Admin",
+  avatarUrl: user?.photo,
+  email: user?.email,
+};
 
   const handleItemClick = () => {
     setMobileOpen(false);
