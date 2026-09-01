40c563b385a514994d38ad122c672507b0cf5e70
GhazliDarine <nm_ghazli@esi.dz>
2026-09-01 15:00:27 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,19 +8,35 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
     VitePWA({
       registerType: "autoUpdate",
+
       manifest: {
         name: "NutriGest",
         short_name: "NutriGest",
-        theme_color: "#4E9F8A", 
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#4E9F8A",
+
         icons: [
-          { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
         ],
       },
+
       workbox: {
-        globPatterns: ["**/*.{js,css,html,svg,png,PNG}"],
+        globPatterns: [
+          "**/*.{js,css,html,svg,png,wav,mp3}",
+        ],
       },
     }),
   ],
@@ -32,9 +48,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
-  // server: {
-  //   host: "192.168.100.12",
-  //   port: 5173,
-  // },
 });
