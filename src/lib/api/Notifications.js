0f801f07d10a478948f15061853b811021396b10
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-09-01 01:44:18 +0100

    Update Notifications.js

--- src/lib/api/Notifications.js
+++ src/lib/api/Notifications.js
@@ -5,4 +5,8 @@ export const getNotifications = (params) => api.get("/api/alertes/notifications/
 
 export const getHistoriqueAlertes = (params) =>api.get("/api/alertes/historique/", { params });
 
-export const exportHistoriqueAlertes = () => api.get("/api/alertes/historique/export/", {  responseType: "blob", });
+export const exportHistoriqueAlertes = (params) =>
+  api.get("/api/alertes/historique/export/", {
+    params,
+    responseType: "blob",
+  });
